f122c28b620a3651cc2485139dffc486c0d52263
NadiaDNA <nadiadininurazizah@gmail.com>
2025-10-15 23:16:01 +0700

    try post for createUser

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const app = express();
 const allRoutes = require('./routes');
 const connectToDb = require('./config/db');
 
+
 const PORT = 3000 || process.env.PORT;
 
 connectToDb();
 
 app.use(express.json());
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.json('Hello World');
-} );
+});
 
 app.use(allRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,24 @@
+const User = require('../models/user.model');
+
 module.exports = {
 getAllUser: (req, res) => {},
 getUserById: (req, res) => {},
-createUser: (req, res) => {},
+createUser: (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+        newUser.save()
+
+        res.status(201).json({message: 'User created successfully'});
+        
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error", error: error.message });
+    }
+},
 updateUser: (req, res) => {},
 deleteUser: (req, res) => {}
 }
