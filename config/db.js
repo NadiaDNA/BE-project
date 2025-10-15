2a6eb8ad5464129d7a97b867911b352e4d8ad9f1
NadiaDNA <nadiadininurazizah@gmail.com>
2025-10-15 23:48:43 +0700

    try get for getAllUser

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDb = async () => {
   try {
-    const connectionString = "mongodb+srv://nadin:nadin@cluster0.og7mj4c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const connectionString = "mongodb+srv://nadin:nadin@cluster0.og7mj4c.mongodb.net/ecourse?retryWrites=true&w=majority&appName=Cluster0";
     await mongoose.connect(connectionString);
     console.log("Successfully connected to MongoDB.");
   } catch (error) {
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,31 @@
 const User = require('../models/user.model');
 
 module.exports = {
-getAllUser: (req, res) => {},
-getUserById: (req, res) => {},
+getAllUser: async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json({message: "Get all users successfully", data: users});
+
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error", error: error.message });
+    }
+},
+getUserById: (req, res) => {
+    try {
+        const { id } = req.params;
+        User.findById(id, (err, user) => {
+            if (err) {
+                return res.status(500).json({message: "Internal Server Error", error: err.message });
+            }
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+            res.status(200).json(user);
+        });
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error", error: error.message });
+    }
+},
 createUser: (req, res) => {
     try {
         const {name, email, password} = req.body;
